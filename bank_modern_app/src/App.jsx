1f019ca5f6eab6abe4e22843e5eac91a674e9879
laney522 <pm72222@gmail.com>
2022-11-25 23:35:22 +0900

    add index.css setting

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import styles from './style';
+//import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
+const App = () => (
+  <div className={`bg-primary`}>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
 
- const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
+
+// const App = () => (
+//   <div className="bg-primary w-full overflow-hidden">
+//     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+//       <div className={`${styles.boxWidth}`}>
+//         Navbar
+//       </div>
+//     </div>
+
+//     <div className={`bg-primary `}>
+
+//     </div>
+
+//   </div>
+// );
+
+// export default App;
