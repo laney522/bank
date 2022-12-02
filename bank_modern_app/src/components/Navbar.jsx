bed84b85c5601945601dab88e7ca9dc893e44b07
laney522 <pm72222@gmail.com>
2022-12-02 22:24:31 +0900

    update navbar.jsx add margin for the li elements

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -12,6 +12,15 @@ const Navbar = () => {
 
       <ul className='list-none sm:flex hidden
       justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px]${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}>
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   )
