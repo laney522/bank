1b1c116d1d7e4550b72af5ec1817f72d193c7193
laney522 <pm72222@gmail.com>
2022-12-12 10:44:37 +0900

    update GetStarted.jsx

--- bank_modern_app/src/components/GetStarted.jsx
+++ bank_modern_app/src/components/GetStarted.jsx
@@ -3,7 +3,8 @@ import { arrowUp } from '../assets';
 
 const GetStarted = () => (
   <div className={`${styles.flexCenter} w-[140px] h-[140px]
-  rounded-full bg-blue-gradient`}>
+  rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}></div>
 
   </div>
 )
