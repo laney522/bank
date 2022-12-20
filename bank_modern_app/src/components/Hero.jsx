e487b3082184e9a529f42a6a2a592314fc4b1b3c
laney522 <pm72222@gmail.com>
2022-12-20 21:48:40 +0900

    update Hero.jsx add GetStarted component for mobile

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => (
           The next <br className="sm:block hidden" /> {" "}
           <span className="text-gradient">Generation</span> {" "}
         </h1>
-        <div className='ss:flex hidden md:mr-4'>
+        <div className='ss:flex hidden md:mr-4 mr-0'>
           <GetStarted />
         </div>
       </div>
@@ -42,12 +42,17 @@ const Hero = () => (
     <div>
       <img src={robot} alt="billing"
         className='w-[100%] h-[100%] relative z-[5]' />
+
       <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
       <div className='absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient' />
       <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
-       
-    </div> 
-  </section> 
+
+    </div>
+
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
+    </div>
+  </section>
 )
 
 export default Hero;
