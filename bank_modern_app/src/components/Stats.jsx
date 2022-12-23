911eb354e7a6dbbb636c19fb590fd50398ecad58
laney522 <pm72222@gmail.com>
2022-12-23 20:35:20 +0900

    update Stats.jsx

--- bank_modern_app/src/components/Stats.jsx
+++ bank_modern_app/src/components/Stats.jsx
@@ -4,8 +4,13 @@ import styles from '../style';
 
 const Stats = () => (
   <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-2- mb-6`}>
-
-  </section>
+    {stats.map((stat) => (
+      <div key={stat.id} className="">
+        <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] xe:leading-[43px] text-white' >{stat.value}</h4>
+        <p>{stat.title}</p>
+      </div>
+    ))}
+  </section >
 )
 
 
