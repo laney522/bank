6cf000657988cdb1dbad7eb763f428cd1b802f9c
laney522 <pm72222@gmail.com>
2022-12-26 21:55:29 +0900

    update Business.jsx & Button.jsx

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -3,6 +3,14 @@ import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button';
 
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div>
+    <div>
+      <img src={icon} alt="icon" />
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
@@ -12,6 +20,14 @@ const Business = () => {
         <p className={`${styles.paragraph} max-2-[470px] mt-5`}>With the right credit card, you can improve your
           financial life by building credit, earning rewards and saving money.
           But with hundreds of credit cards on the market.</p>
+
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
     </section>
   )
