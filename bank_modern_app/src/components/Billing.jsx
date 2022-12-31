0a3e43cfce5a5a68cadb5687d1f8754ccd250b85
laney522 <pm72222@gmail.com>
2022-12-31 20:29:32 +0900

    update Billing.jsx add left-side gradient

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -6,7 +6,10 @@ const Billing = () => {
   return (
     <section id="product" className={layout.sectionReverse}>
       <div className={layout.sectionImgReverse}>
-        <img src={bill} alt="billing" className='w-[100%] h-[100%]' />
+        <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]' />
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
       </div>
     </section>
   )
