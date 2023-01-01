0319d7c83ce2e53d095954bf8fbb4c13ce412569
laney522 <pm72222@gmail.com>
2023-01-01 19:05:48 +0900

    Billing.jsx

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -10,6 +10,16 @@ const Billing = () => {
 
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+
+        <div className={layout.sectionInfo}>
+          <h2 className={styles.heading2}>Easily control your <br
+            className="sm:block hidden" /> billing & invoicing.</h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Elit enim sed massa etiam. Mauris enadipiscing
+            ultrices ametodio aenean neque. Fusce ipsum orci
+            rhoncus aliporttitor integer platea placerat.
+          </p>
+        </div>
       </div>
     </section>
   )
