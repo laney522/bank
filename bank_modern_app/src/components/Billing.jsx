34308741869b5af0379446fbfb4ce6cecc271947
laney522 <pm72222@gmail.com>
2023-01-03 23:19:48 +0900

    cteated main feature of website

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -22,7 +22,10 @@ const Billing = () => {
         </p>
 
         <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-          <img src={apple} alt="google_play" className="w-[128px]" />
+          <img src={apple} alt="apple_store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+          <img src={google} alt="google_store"
+            className="w-[128px] h-[42px] object-contain cursor-pointer" />
         </div>
       </div>
     </section>
