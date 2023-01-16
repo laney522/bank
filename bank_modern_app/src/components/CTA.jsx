e602d617fef4544101891ff58c797e9c885d19f1
laney522 <pm72222@gmail.com>
2023-01-16 22:03:05 +0900

    update

--- bank_modern_app/src/components/CTA.jsx
+++ bank_modern_app/src/components/CTA.jsx
@@ -7,12 +7,12 @@ const CTA = () => (
     <div className='flex-1 flex flex-col'>
       <h2 className={styles.heading2}>Let's try our service now!</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept car payments and
-        grow your business anywhere on the planet.
+        grow your business anywhere on the planet.  
       </p>
     </div>
 
-    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-      <Button />
+    <div className={`${styles.flexCenter}  sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button  />
     </div>
   </section>
 )
