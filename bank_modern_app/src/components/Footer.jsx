a1d3596f8504b686c62d7d353b8681d99f595e4f
laney522 <pm72222@gmail.com>
2023-01-22 12:37:07 +0900

    Footer.jsx update

--- bank_modern_app/src/components/Footer.jsx
+++ bank_modern_app/src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => (
                   hover:text-secondary cursor-pointer 
                   ${index !== footerLink.links.length - 1 ? 'mb-4' : 'md-0'}`}
                 >
-                  {link.name}
+                  { link.name }
                 </li>
               ))}
             </ul>
@@ -46,11 +46,11 @@ const Footer = () => (
     md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
       <p className='font-poppins font-normal text-center 
       text-[18px] leading-[27px] text-white'>
-        2023 HooBank. All Rights Reserved.
+        2023 HooBank. All Rights Reserved. 
       </p>
 
       <div className='flex flex-row md:mt-0 mt-6'>
-        {socialMedia.map((social, index) => (
+        {socialMedia.map(( social, index ) => (
           <img
             key={social.id}
             src={social.icon}
